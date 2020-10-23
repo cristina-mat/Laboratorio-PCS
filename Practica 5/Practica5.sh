32a3cc51b5efb62c192e57bda8eb94b32e6ba811
Cristina <cmata4628@gmail.com>
2020-10-23 16:30:51 -0400

    Practica 5 agregada

---
+++ Practica 5/Practica5.sh
@@ -0,0 +1,4 @@
+base64 --decode < mystery_img1.txt > mystery_img1.png
+base64 --decode < mystery_img2.txt > mystery_img2.png
+base64 < fcfm.png > fcfm_encoded.txt
+base64 < msg.txt > msg_encoded.txt
